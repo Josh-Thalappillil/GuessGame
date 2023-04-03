defe154bde7bf5901141dd11e2e101c5988f063f
Josh <jthalappillil7@gmail.com>
2023-04-03 20:29:42 +1000

    Removed method from ConsoleMsg.cs
    Removed redundant method from ConsoleMsg.cs and created Guess Game repository. Previous commits are located on percipio learning.

---
+++ GameEvents.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessGame
+{
+    public class GameEvents
+    {
+        int RandomNum;
+        
+        public int NumGenerator()
+        {
+            Random random = new Random();
+            RandomNum = random.Next(1, 11);
+            return RandomNum;
+        }
+        
+        public void CheckAnswer()
+        {
+            GameEvents gameEvents = new GameEvents();
+            gameEvents.NumGenerator();
+
+            int inputCount = 0;
+            int UserInput;
+
+            while (true)
+            {
+                Console.WriteLine("\nEnter your guess:");
+                UserInput = Convert.ToInt32(Console.ReadLine());
+                inputCount++;
+
+                                
+                if (UserInput == gameEvents.RandomNum)
+                {
+                    Console.WriteLine("CONGRATULATIONS YOU WIN!");
+                    break;
+                }
+                else if (UserInput < gameEvents.RandomNum)
+                {
+                    Console.WriteLine("You guessed too low, try again.");
+                    continue;
+                }
+                else if (UserInput > gameEvents.RandomNum) 
+                {
+                    Console.WriteLine("You guessed too high, try again.");
+                    continue;
+                }
+            }
+            Console.WriteLine($"You took {inputCount} tries to get the answer!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using GuessGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+var simulate = new ConsoleMsg();
+var simulate2 = new GameEvents();
+simulate.WelcomeMsg();
+simulate2.CheckAnswer();
